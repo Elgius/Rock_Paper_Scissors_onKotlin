03267ee1876524d3ee4d7005cf9e70e2d99f5350
Elgius <114131653+Elgius@users.noreply.github.com>
2022-12-06 13:41:04 +0500

    Finally done!

--- src/main/kotlin/RPS.kt
+++ src/main/kotlin/RPS.kt
@@ -4,14 +4,14 @@ fun gameLogic(){
     val userInput: String = readln()
     val option: String = userInput.uppercase()
     println("Your choice: $userInput")
-    val cplayerChoice = arrayOf("R","S","P").random()
-    println("Computer picked: $cplayerChoice")
-    println("This is your choice: $option and this is the Computers choice: $cplayerChoice")
+    val computerPlayer = arrayOf("R","S","P").random()
+    println("Computer picked: $computerPlayer")
+    println("This is your choice: $option and this is the Computers choice: $computerPlayer")
 
 
     when (option){
 
-        "R","S","P" -> println(results(option,cplayerChoice))
+        "R","S","P" -> println(results(option,computerPlayer))
         else -> print("you made some invalid Input")
     }
 }
@@ -21,54 +21,48 @@ fun gameLogic(){
 
 fun results(data: String, choice: String){
 
-    var tie:Int = 0
-    var user:Int = 0
-    var computer:Int = 0
+    var final = ""
 
-    var final: String = ""
-
-    //while (i = rounds)
-
-    if (data == "R"){
-        if(choice == "P"){
-            final = "Computer wins"
-            losses += 1
+        if (data == "R") {
+            if (choice == "P") {
+                final = "Computer wins"
+                losses += 1
+            } else {
+                final = "You won"
+                wins += 1
+            }
+        } else if (data == "P") {
+            if (choice == "S") {
+                final = "Computer wins"
+                losses += 1
+            } else {
+                final = "You won"
+                wins += 1
+            }
+        } else if (data == "S") {
+            if (choice == "R") {
+                final = "Computer wins"
+                losses += 1
+            } else {
+                final = "You won"
+                wins += 1
+            }
         }
-        else {final = "You won"
-            wins += 1 }
-    }
-
-    else if(data == "P"){
-        if(choice == "S"){
-            final = "Computer wins"
-            losses += 1
-        }
-        else { final = "You won"
-            wins += 1 }
-    }
-
-    else if(data == "S"){
-        if(choice == "R"){
-            final = "Computer wins"
-            losses += 1
-        }
-        else { final = "You won"
-            wins += 1 }
-    }
-
     println(final)
-    println("wins: $wins , ties:  $ties, loss:  $losses")
+    println("wins: $wins , ties:  $ties, loss: $losses")
 }
 
 fun main(){
-    println("Welcome to the game my dudessss")
+    println("Welcome to the game my dude")
 
     println("How many rounds do you want to play?")
     val round:String = readln()
     val rounds = round.toInt()
+    var i = 0
 
-    for(i in 1..rounds){
+
+    while (i<rounds) {
         gameLogic()
+        i++
     }
-
 }
